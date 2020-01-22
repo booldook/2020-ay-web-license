248634a25aa676f205f4a0e5f02358d822f2027b
booldook <booldook@gmail.com>
2020-01-22 12:02:16 +0900

    nav.html

--- script/nav.js
+++ script/nav.js
@@ -18,3 +18,24 @@ $(".header3 .navi").mouseover(function(){
 $(".header3").mouseleave(function(){
 	$(this).find(".navi-wrap").stop().slideUp(300);
 });
+
+var now = 0;
+//setInterval(ani, 4000);
+$(".prev").click(function(){
+	if(now > 0) {
+		now--;
+		ani();
+	}
+});
+$(".next").click(function(){
+	if(now < 2) {
+		now++;
+		ani();
+	}
+});
+function ani() {
+	$(".ban").stop().fadeOut(500);
+	$(".ban").eq(now).stop().fadeIn(500);
+	/* $(".ban").stop().animate({"opacity": 0}, 500);
+	$(".ban").eq(now).stop().animate({"opacity": 1}, 500); */
+}
